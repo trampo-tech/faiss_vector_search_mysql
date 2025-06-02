ca34f99ee2682806b599cfa831dfaaabe6c0c2dc
Matheus Girardi <matheus.girardi@brf.com>
2025-06-02 14:17:23 -0300

    feat: add method to add/update item in faiss index

--- app/faiss/faissManager.py
+++ app/faiss/faissManager.py
@@ -2,6 +2,9 @@ import faiss
 import numpy
 from sentence_transformers import SentenceTransformer
 from app.config.config import Config
+import logging
+
+logger = logging.getLogger()
 
 class Faiss_Manager:
     def __init__(self, dimensionality: int):
@@ -33,16 +36,46 @@ class Faiss_Manager:
                 if field in item and item[field] is not None:
                     texts_to_join.append(str(item[field]))
                 else:
-                    print(f"Warning: Field '{field}' not found or is None in item with id {item.get('id', 'Unknown')}. Skipping field.")
+                    logger.warning(f"Warning: Field '{field}' not found or is None in item with id {item.get('id', 'Unknown')}. Skipping field.")
             
             if not texts_to_join:
-                print(f"Warning: No text could be extracted for item with id {item.get('id', 'Unknown')} using fields {text_fields}. Skipping item.")
+                logger.warning(f"No text could be extracted for item with id {item.get('id', 'Unknown')} using fields {text_fields}. Skipping item.")
                 continue
 
             text_to_embed = " ".join(texts_to_join)
             item_id = item["id"]
             self._add_text(text_to_embed, item_id)
 
+    def add_or_update_item(self, item: dict, text_fields: list[str] = ["titulo", "descricao"]):
+        if "id" not in item:
+            raise ValueError(f"Item does not have an 'id' field. Received: {item}")
+
+        item_id = item["id"]
+
+        # Concatenate text from specified fields
+        texts_to_join = []
+        for field in text_fields:
+            if field in item and item[field] is not None:
+                texts_to_join.append(str(item[field]))
+            else:
+                logger.warning(f"Field '{field}' not found or is None in item with id {item_id}. Skipping field.")
+        
+        if not texts_to_join:
+            raise ValueError(f"Warning: No text could be extracted for item with id {item_id} using fields {text_fields}. Skipping item.")
+            
+
+        text_to_embed = " ".join(texts_to_join)
+
+        # Remove the old entry if it exists
+        # FAISS expects IDs to be a numpy array of int64 for IDSelectorArray
+        ids_to_remove_np = numpy.array([item_id], dtype=numpy.int64)
+        # Create an IDSelector for the ID to be removed
+        # The first argument to IDSelectorArray is the number of IDs, 
+        # the second is a pointer to the C-array of IDs.
+        selector = faiss.IDSelectorArray(ids_to_remove_np.shape[0], faiss.swig_ptr(ids_to_remove_np))
+        self.index.remove_ids(selector) # type: ignore 
+
+        self._add_text(text_to_embed, item_id)
 
     def search_text(self, text:str, top_k:int=5):
         embedding = self.embedding_model.encode([text])
