ec601a1db9836a3d65a79f4414be7b0ee5dc503c
Matheus Girardi <matheus.girardi@brf.com>
2025-06-02 14:18:02 -0300

    feat: new centralized index configuration

--- app/config.py
+++ app/config.py
@@ -14,11 +14,20 @@ class Config:
     embed_model = "sentence-transformers/all-MiniLM-L6-v2"
     indexes_dir = "indexes"
 
-
     tables_to_index: List[TableConfig] = [
         TableConfig(name="usuarios", columns=["nome"], hybrid=False),
-        TableConfig(name="itens", columns=["titulo", "descricao", "condicoes_uso"], hybrid=True),
+        TableConfig(
+            name="itens", columns=["titulo", "descricao", "condicoes_uso"], hybrid=True
+        ),
     ]
+
+    @classmethod
+    def get_table_config(cls, table_name: str) -> TableConfig:
+        for table in cls.tables_to_index:
+            if table.name == table_name:
+                return table
+        else:
+            raise Exception(f"Did not find config for {table_name}")
 
     class MySQL:
         user = "root"
@@ -31,8 +40,6 @@ class Config:
         logging.basicConfig(
             level=logging_level,
             format="%(asctime)s - %(levelname)s - %(message)s",
-            handlers=[
-                logging.StreamHandler()
-            ]
+            handlers=[logging.StreamHandler()],
         )
         return logging.getLogger()
